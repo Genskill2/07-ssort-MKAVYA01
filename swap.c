8bb311a4f5668b3815e81ff14161630bef4701e6
M Kavya <73734974+MKAVYA01@users.noreply.github.com>
2021-07-01 23:25:30 +0530

    swap

---
+++ swap.c
@@ -0,0 +1,17 @@
+void swap_max(int arr[],int l,int n) {
+  int temp,max=n;
+  for(int i=n;i<l;i++) {
+    if(arr[i]>arr[max]) {
+      max = i;
+    }
+  }
+  temp = arr[max];
+  arr[max] = arr[n];
+  arr[n] = temp;
+}
+
+void ssort(int arr[],int l) {
+  for(int i=0;i<l;i++) {
+    swap_max(arr,l,i);
+  }
+}
